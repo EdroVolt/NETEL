dde971c8c2cff7873ce02e8228619ea40596e09b
Edro <ahmededrees30@gmail.com>
2020-07-05 15:53:32 +0200

    feat: add click event on recent projects images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+const recentProjectRows = document.querySelectorAll(".recent-projects .row");
+const recentProjectsSection = document.querySelector(".recent-projects");
+const body = document.querySelector("body");
 
+for (recentProjectRow of recentProjectRows) {
+    recentProjectRow.addEventListener('click', showBiggerImage);
+}
+
+function showBiggerImage(event) {
+    const image = event.target;
+    console.log(image);
+
+    // create a copy from the image element
+    const biggerImage = image.cloneNode(true);
+
+    // create box
+    const box = document.createElement("div");
+    box.classList.add("image-box");
+
+    // create close button
+    const btnClose = document.createElement("button");
+    btnClose.classList.add("btn-close", "btn", "btn-dark", "btn-outline-secondary", "m-2");
+    btnClose.textContent = "X";
+    btnClose.addEventListener('click', () => {
+        box.remove();
+    });
+
+    // append image and button to box
+    box.appendChild(biggerImage);
+    box.appendChild(btnClose);
+
+    // append box to DOM
+    recentProjectsSection.appendChild(box);
+}
